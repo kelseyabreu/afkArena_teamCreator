3f9e8f8df369679e460220f32dc3b5b0b21f9c08
Kelsey Abreu <Kelseyabreu@hotmail.com>
2024-10-01 15:43:46 -0400

    init commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { DragDropModule } from '@angular/cdk/drag-drop';
+
+import { AppComponent } from './app.component';
+import { IconTeamOrganizerComponent } from './icon-team-organizer/icon-team-organizer.component';
+import { DisplayPageComponent } from './display-page/display-page.component';
+import { IconTeamService } from './services/icon-team.service';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    IconTeamOrganizerComponent,
+    DisplayPageComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    DragDropModule
+  ],
+  providers: [IconTeamService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
+//sssd
